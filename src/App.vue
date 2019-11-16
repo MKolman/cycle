f17e850c29b7bc0a049b177a4b726bc61ed5985f
Maks Kolman <kolman.maks@gmail.com>
2019-11-16 21:07:26 +0000

    Pin down the app outline

--- src/App.vue
+++ src/App.vue
@@ -1,61 +1,81 @@
 <template>
-  <v-app>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+      clipped
+      overflow
+    >
+      <v-list dense>
+        <v-list-item
+          link
+          to="/"
+        >
+          <v-list-item-action>
+            <v-icon>mdi-home</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item
+          link
+          to="/about"
+        >
+          <v-list-item-action>
+            <v-icon>mdi-information</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>About</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
     <v-app-bar
       app
-      color="primary"
+      color="indigo"
       dark
+      clipped-left
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      <v-img
+        alt="Tracker logo"
+        class="shrink mr-2"
+        contain
+        src="/img/icons/icon.svg"
+        transition="scale-transition"
+        width="40"
+      />
+      <v-toolbar-title>Tracker</v-toolbar-title>
     </v-app-bar>
 
     <v-content>
-      <HelloWorld/>
+      <v-container
+        class="fill-height"
+        fluid
+      >
+        <router-view />
+      </v-container>
     </v-content>
+    <v-footer
+      color="indigo"
+      app
+    >
+      <span class="white--text">Made by Maks Kolman &copy; 2019</span>
+    </v-footer>
   </v-app>
 </template>
 
-<script lang="ts">
-import Vue from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+  export default {
+    props: {
+      source: String,
+    },
 
-export default Vue.extend({
-  name: 'App',
-
-  components: {
-    HelloWorld
-  },
-
-  data: () => ({
-    //
-  })
-})
+    data: () => ({
+      drawer: null,
+    }),
+  }
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,122 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>
+      {{ currentMonthName }}
+      <span class="navigation">
+        <v-btn
+          fab
+          text
+          large
+          @click="prev"
+        >
+          <v-icon large>mdi-chevron-left</v-icon>
+        </v-btn>
+        <v-btn
+          fab
+          text
+          large
+          @click="next"
+        >
+          <v-icon large>mdi-chevron-right</v-icon>
+        </v-btn>
+      </span>
+    </h1>
+    <v-calendar
+      ref="calendar"
+      v-model="currentDate"
+      class="elevation-2"
+      type="month"
+      :events="allEvents"
+      :event-color="getEventColor"
+      @click:event="handleEvent"
+      @click:date="handleDate"
+      @click:day="handleDay"
+    />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  export default {
+    name: 'Home',
+    data () {
+      return {
+        creatingEvent: false,
+        currentDate: '2019-11-15',
+        newEvent: {
+          name: 'New Event',
+          color: 'primary',
+        },
+        events: [
+          {
+            name: 'Krvavec',
+            start: '2018-11-01',
+            end: '2019-11-10',
+          },
+        ],
+      }
+    },
+    computed: {
+      allEvents () {
+        if (this.creatingEvent) {
+          return [this.newEvent].concat(this.events)
+        }
+        return this.events
+      },
+      defaultColor () {
+        return ['primary', 'secondary'][+this.creatingEvent]
+      },
+      currentMonthName () {
+        let date = new Date(this.currentDate)
+        console.log(date)
+        return date.toLocaleString('default', { month: 'long', year: 'numeric' })
+      },
+    },
+    methods: {
+      getEventColor (event) {
+        return event.color || this.defaultColor
+      },
+      handleEvent (event) {
+        console.log('Event', event)
+        console.log('Event', this.events)
+      },
+      handleDay (event) {
+        if (this.creatingEvent) {
+          this.handleDate(event)
+        }
+      },
+      handleDate (event) {
+        if (this.creatingEvent) {
+          let ev = { name: '', start: this.newEvent.start, end: event.date }
+          if (ev.start > ev.end) [ev.start, ev.end] = [ev.end, ev.start]
+          this.events.push(ev)
+        } else {
+          this.newEvent.start = event.date
+          this.newEvent.end = event.date
+        }
+        this.creatingEvent = !this.creatingEvent
+        console.log('Day', event)
+      },
+      prev () {
+        this.$refs.calendar.prev()
+      },
+      next () {
+        this.$refs.calendar.next()
+      },
+    },
   }
-}
 </script>
+
+<style scoped lang="scss">
+  .home {
+    width: 100%;
+    height: 100%;
+    padding-bottom: 4em;
+    h1 {
+      line-height: 2em;
+      .navigation {
+        white-space: nowrap;
+        float: right;
+      }
+    }
+  }
+</style>
