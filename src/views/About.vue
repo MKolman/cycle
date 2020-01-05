6097216cbe04c906cc771ef6787ca07fda49a982
Maks Kolman <kolman.maks@gmail.com>
2020-01-05 13:14:26 +0000

    Add about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About</h1>
+    <p>
+      Tracker is a web app for recording and predicting periodic events (e.g. menstrual cycles). There is no server and no other person that has access to your data, because everything is stored on your device.
+    </p>
+    <p>
+      The website was made by Maks Kolman with the help of <a href="https://www.pavlaguitar.com">Pavla Lusin</a> and is hosted on <a href="https://www.github.com/MKolman/tracker">GitHub</a>.
+    </p>
   </div>
 </template>
